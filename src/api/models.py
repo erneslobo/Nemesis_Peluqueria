b3de9685c3c6b6a4419a17999b1b43deac8c13d3
erneslobo <erneslobo@gmail.com>
2021-11-05 03:33:45 +0000

    Creacion de Modelos y algunos Enpoints

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,133 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+# Creacion de los modelos de la base de datos
+
+#Creacion del modelo de Usuario
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    nombre = db.Column(db.String(250), nullable=False)
+    apellido = db.Column(db.String(250), nullable=False)
+    telefono = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False)
+    favoritos = db.relationship('Favoritos', backref='usuario', lazy=True)
+    ordenes = db.relationship('Orden', backref='usuario', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<Usuario {self.nombre}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "telefono": self.telefono,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "admin": self.admin
+            # no se serializa el password por motivos de seguridad
+        }
+
+#Creacion del modelo de Producto
+class Producto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250), unique=True, nullable=False)
+    descripcion = db.Column(db.Text, nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    tipo = db.Column(db.String(250), nullable=False)
+    categoria = db.Column(db.String(250), nullable=False)
+    imagen = db.Column(db.String(250), nullable=False)
+    activo = db.Column(db.Boolean(), nullable=False)
+    detalle_ordenes = db.relationship('DetalleOrden', backref='producto', lazy=True)
+
+    def __repr__(self):
+        return f'<Producto {self.nombre}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "descripcion": self.descripcion,
+            "precio": self.precio,
+            "tipo": self.tipo,
+            "categoria": self.categoria,
+            "imagen": self.imagen,
+            "activo": self.activo
+        }
+
+#Creacion del modelo de Muestra
+class Muestra(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(250), unique=True, nullable=False)
+    imagen = db.Column(db.String(250), nullable=False)
+    categoria = db.Column(db.String(250), nullable=False)
+    activo = db.Column(db.Boolean(), nullable=False)
+    favoritos = db.relationship('Favoritos', backref='muestra', lazy=True)
+
+    def __repr__(self):
+        return f'<Muestra {self.nombre}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "imagen": self.imagen,
+            "categoria": self.categoria,
+            "activo": self.activo
+        }
+
+#Creacion del modelo de Orden
+class Orden(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    metodo_pago = db.Column(db.String(250), nullable=False)
+    monto_total = db.Column(db.Float, nullable=False)
+    detalle_ordenes = db.relationship('DetalleOrden', backref='orden', lazy=True)
+
+    def __repr__(self):
+        return f'<Orden {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuario_id,
+            "metodo_pago": self.metodo_pago,
+            "monto_total": self.monto_total
+        }
+
+#Creacion del modelo de Detalle de Orden
+class DetalleOrden(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    orden_id = db.Column(db.Integer, db.ForeignKey('orden.id'), nullable=False)
+    producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<DetalleOrden {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "orden_id": self.orden_id,
+            "producto_id": self.producto_id,
+            "cantidad": self.cantidad
+        }
+
+#Creacion del modelo de Favoritos
+class Favoritos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    muestra_id = db.Column(db.Integer, db.ForeignKey('muestra.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Favoritos {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuario_id,
+            "muestra_id": self.muestra_id,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,115 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Usuario, Producto, Muestra, Orden, DetalleOrden, Favoritos
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
+@api.route('/usuarios', methods=['GET'])
+def get_usuarios():
+    usuarios_query = Usuario.query.all()
+    all_usuarios = list(map(lambda x: x.serialize(), usuarios_query))
+    return jsonify(all_usuarios), 200
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/muestras', methods=['GET'])
+def get_muestras():
+    muestras_query = Muestra.query.all()
+    all_muestras = list(map(lambda x: x.serialize(), muestras_query))
+    return jsonify(all_muestras), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend"
-    }
+@api.route('/productos', methods=['GET'])
+def get_productos():
+    productos_query = Producto.query.all()
+    all_productos = list(map(lambda x: x.serialize(), productos_query))
+    return jsonify(all_productos), 200
 
-    return jsonify(response_body), 200
+@api.route('/favoritos', methods=['GET'])
+@jwt_required()
+def get_usuario_favoritos():
+    identidad_usuario = get_jwt_identity()
+    usuario = Usuario.query.filter_by(email=identidad_usuario).first()
+    usuario_favoritos = Favoritos.query.filter_by(usuario_id=usuario.id).all()
+    usuario_favoritos = list(map(lambda x: x.serialize(), usuario_favoritos))
+    return jsonify(usuario_favoritos)
+
+@api.route('/registro', methods=['POST'])
+def agregar_usuario():
+    usuario = Usuario.query.filter_by(email=request.json.get("email", None)).first()
+    if usuario is not None:
+        raise APIException('Usuario ya existe!', status_code=409)
+
+    nombre = request.json.get("nombre", None)
+    apellido = request.json.get("apellido", None)
+    telefono = request.json.get("telefono", None)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    admin = request.json.get("admin", False)
+
+    usuario = Usuario(nombre=nombre,
+                apellido= apellido,
+                telefono = telefono,
+                email=email,
+                password=password,
+                admin = admin
+                )
+    db.session.add(usuario)
+    db.session.commit()
+
+    usuario = Usuario.query.filter_by(email=email).first()
+    usuario = usuario.serialize()
+    return jsonify(usuario), 201
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    usuario = Usuario.query.filter_by(email=email, password=password).first()
+    if usuario is None:
+        raise APIException('Usuario o password incorrecto', status_code=401)
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route('/favoritos/<int:muestra_id>', methods=['POST'])
+@jwt_required()
+def agregar_favoritos(muestra_id):
+    identidad_usuario = get_jwt_identity()
+    usuario = Usuario.query.filter_by(email=identidad_usuario).first()
+    existe_muestra = Muestra.query.get(muestra_id) is not None
+    existe_usuario = usuario is not None
+    if not existe_muestra:
+        raise APIException('Favorito no existe', status_code=404)
+    if not existe_usuario:
+        raise APIException('Usuario no existe', status_code=404)
+    
+    existe_favorito = Favoritos.query.filter_by(usuario_id=usuario.id, muestra_id=muestra_id).first() is not None
+    if not existe_favorito:
+        favorito = Favoritos(usuario_id=usuario.id, muestra_id=muestra_id)
+        db.session.add(favorito)
+        db.session.commit()
+        return get_usuario_favoritos()
+    else:
+        raise APIException('Favorito ya existe', status_code=409)
+
+@api.route('/favoritos/<int:muestra_id>', methods=['DELETE'])
+@jwt_required()
+def borrar_favoritos(muestra_id):
+    identidad_usuario = get_jwt_identity()
+    usuario = Usuario.query.filter_by(email=identidad_usuario).first()
+    existe_muestra = Muestra.query.get(muestra_id) is not None
+    existe_usuario = usuario is not None
+    if not existe_muestra:
+        raise APIException('Favorito no existe', status_code=404)
+    if not existe_usuario:
+        raise APIException('Usuario no existe', status_code=404)
+    
+    favorito = Favoritos.query.filter_by(usuario_id=usuario.id, muestra_id=muestra_id).first()
+    existe_favorito = favorito is not None
+    if existe_favorito:
+        db.session.delete(favorito)
+        db.session.commit()
+        return get_usuario_favoritos()
+    else:
+        raise APIException('Favorito no existe', status_code=404)

d7e1cae43d8a9a6a9fb95aee8d156c50c7c84fe4
acarnicelli <alecarnicelli@hotmail.com>
2021-11-06 02:58:14 +0000

    footer terminado?

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,23 +2,29 @@ import React, { Component } from "react";
 import facebook_logo from "../../img/facebook_logo.png";
 import instagram_logo from "../../img/instagram_logo.png";
 import whatsapp_logo from "../../img/whatsapp_logo.png";
+import nemesis_logo from "../../img/nemesis_completo_blanco.png";
 import "../../styles/footer.scss";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<div className="container-fluid">
 			<div className="container">
-				<div className="row">
-					<div className="col-3"> Logo</div>
-					<div className="col-3">nada</div>
-					<div className="col-3">nada</div>
-					<div className="col-3">nada</div>
+				<div className="row primer-row">
+					<div className="col-4 logoPelu">
+						<img src={nemesis_logo} alt="" />
+					</div>
+					<div className="col-4" />
+					<div className="col-4" />
 				</div>
 				<div className="row">
-					<div className="col-3"> texto</div>
-					<div className="col-3">nada</div>
-					<div className="col-3">nada</div>
+					<div className="col-3 datos">
+						<p>Chucarro 1146 esq. Guayaqui</p>
+						<p>Martes a Sábados de 10:00 a 20:00 hs</p>
+					</div>
+					<div className="col-3" />
+					<div className="col-3" />
 					<div className="col-3 redes">
+						<div className="text">Encuéntranos en: </div>
 						<div className="facebook">
 							<img src={facebook_logo} alt="" />
 						</div>

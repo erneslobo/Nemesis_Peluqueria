ba1b899e7b28786c529f6485fce2b65cc60242e0
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-11-06 14:50:41 +0000

    Page of register initial

--- src/front/js/pages/registro.js
+++ src/front/js/pages/registro.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import "../../styles/registro.scss";
 
 export const Registro = () => {
-	return <p />;
+	return (
+		<>
+			<div className="mega ">
+				<div className="container box">
+					<div id="div1">
+						<h4>Registrarse</h4>
+						<div className="mb-3">
+							<input type="email" className="form-control" placeholder="Email" />
+						</div>
+						<div className="mb-3">
+							<input type="password" className="form-control" placeholder="Contraseña" />
+							<p>* La contraseña debe tener entre 6 y 8 caracteres</p>
+						</div>
+						<div className="col-auto">
+							<button type="submit" className="btn btn-dark mb-3">
+								Registrarse
+							</button>
+						</div>
+						<Link to={`/login`}>{"Iniciar sesion"}</Link>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };

ddb485fcf2069d3bf8dddc3b00fe9e4043f822ee
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-11-18 00:02:33 +0000

    comenté el modal

--- src/front/js/pages/productos.js
+++ src/front/js/pages/productos.js
@@ -57,3 +57,14 @@ export const Productos = () => {
 		</div>
 	);
 };
+
+/* MODAL */
+/* <!-- Vertically centered modal -->
+<div class="modal-dialog modal-dialog-centered">
+  ...
+</div>
+
+<!-- Vertically centered scrollable modal -->
+<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+  ...
+</div>*/
